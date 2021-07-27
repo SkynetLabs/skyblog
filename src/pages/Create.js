c0262b20e51b94af5f304750be09625ad952f8b3
Kevin Carlson <kevincarlson@Kevins-MacBook-Pro.local>
2021-07-27 14:52:09 -0500

    added comments to create

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -16,7 +16,7 @@ import InsertLink from '@material-ui/icons/InsertLink';
 import Close from '@material-ui/icons/Close';
 import CreateMenu from "../components/CreateMenu";
 import { createEditor, Editor, Transforms, Text, Range } from "slate";
-import {Slate, Editable, withReact, useSlate, ReactEditor} from "slate-react";
+import {Slate, Editable, withReact, useSlate, ReactEditor, useSelected} from "slate-react";
 import { useTheme } from '@material-ui/core/styles';
 
 
@@ -57,24 +57,31 @@ export default function Create (props) {
     const classes = useStyles();
 
     //states to track store the blog text
-    const [title, setTitle] = useState();
-    const [subtitle, setSubtitle] = useState();
+    const [title, setTitle] = useState(); //blog title
+    const [subtitle, setSubtitle] = useState(); //blog subtitle
     const [blogBody, setBlogBody] = useState([{
         type: 'paragraph',
         children: [{ text: '' }],
-    },]);
+    },]); //state used by slate editor when blog body changes
 
+    //initialization of the slate editor reference
     const editorRef = useRef();
     if (!editorRef.current) editorRef.current = withReact(createEditor());
     const editor = editorRef.current;
 
+    //handle the title input change
     const handleTitleChange = (event) => {
+
         setTitle(event.target.value);
     }
+
+    //handle the subtitle input change
     const handleSubtitleChange = (event) => {
         setSubtitle(event.target.value);
     }
 
+    //render each element for slate editor based on type
+    //supported types: 'code', 'title', 'subtitle', 'image', default: 'paragraph'
     const renderElement = useCallback(props => {
         switch (props.element.type) {
             case 'code':
@@ -83,17 +90,24 @@ export default function Create (props) {
                 return <TitleElement {...props} />
             case 'subtitle':
                 return <SubtitleElement {...props} />
+            case 'image':
+                return <ImageElement {...props} />
             default:
                 return <DefaultElement {...props} />
         }
     }, []);
 
+    //render function for so-called "leaves" of each element
+    //allows for different formatting within line items i.e. bold, italics, underlined, link
     const renderLeaf = useCallback(props => {
         return <Leaf {...props} />
     }, []);
 
+    //return JSX for create page
+    //Title Input, Subtitle Input
+    //Slate editor with hovering toolbar (shows on selection) and inline menu (<CreateMenu/>)
     return (
-        <Container maxWidth={'sm'} style={{marginTop:14}}>
+        <Container id={'create-container'} maxWidth={'sm'} style={{marginTop:14}}>
             <Grid container direction={'row'} alignItems={'center'} >
                 <TextField label={'Title'} fullWidth={true} autoFocus={true} multiline={true} rowsMax={3}
                            onChange={handleTitleChange}
@@ -119,6 +133,7 @@ export default function Create (props) {
                    >
                 <HoveringToolbar />
                 <Editable
+                    id={'editor'}
                     renderElement={renderElement}
                     renderLeaf={renderLeaf}
                     placeholder={'Tell your story...'}
@@ -158,13 +173,12 @@ export default function Create (props) {
                     }}
                 />
             </Slate>
-
-
-
+            <CreateMenu addImageElement={addImageElement} editor={editor}/>
         </Container>
     );
 }
 
+//render function for 'code' element type
 const CodeElement = props => {
     return (
         <pre {...props.attributes}>
@@ -172,26 +186,37 @@ const CodeElement = props => {
     </pre>
     )
 }
-
+//render function for default element type
 const DefaultElement = props => {
     return <p {...props.attributes}>{props.children}</p>
 }
 
+//render function for 'title' element type
 const TitleElement = props => {
     return <p style={{fontSize: 40, fontStyle:'bold'}} {...props.attributes}>{props.children}</p>
 }
+
+//render function for 'subtitle' element type
 const SubtitleElement = props => {
     return <p style={{fontSize: 30, fontStyle:'bold'}} {...props.attributes}>{props.children}</p>
 }
+
+//render function for 'link' leaf
 const LinkElement = props => {
     const textRef = useRef(null);
     const [popoverAnchor, setPopoverAnchor] = useState(null);
+
+    //open popover showing link address on hover
     const handleOpenPop = (event) => {
         setPopoverAnchor(event.currentTarget);
     }
+
+    //hide popover
     const handleClosePop = () => {
         setPopoverAnchor(null)
     }
+
+    //open link in new tab on click, parse in https:// as necessary
     const handleClick = () => {
         if (props.leaf.linkUrl.startsWith('https://')){
             window.open(props.leaf.linkUrl);
@@ -200,6 +225,7 @@ const LinkElement = props => {
         }
 
     }
+    //JSX returning link format and popover
     return (
         <>
             <Typography
@@ -224,6 +250,7 @@ const LinkElement = props => {
     );
 }
 
+//render leaf function, with appropriate styling applied
 const Leaf = props => {
     return (
         <span
@@ -238,8 +265,36 @@ const Leaf = props => {
     )
 }
 
+//render image function, centered horizontally and maxHeight of 1/3 page height
+const ImageElement = props => {
+    const selected = useSelected();
+    return (
+        <Container
+            style={{display:'flex', justifyContent:'center'}}>
+            <div contentEditable={false}>
+                <img src={props.element.src} style={{maxHeight:window.innerHeight/3, border: selected ? '4px solid #EBEFEE' : null}}
+                />
+            </div>
+            {props.children}
+        </Container>
+    )
+}
+
+//handler for when an image is selected from the CreateMenu
+//inserts image and empty default element to cursor location in document
+const addImageElement = (editor, img) => {
+    Transforms.insertNodes(editor,
+        [{type:'image', src: img, children:[{text: ''}]},
+            {type:'paragraph', children:[{text:''}]}]);
+    console.log(editor);
+}
+
+//activate/deactivate specified format
+
 const toggleFormat = (editor, format, linkUrl = null) => {
+    //check current status of selection
     const isActive = isFormatActive(editor, format);
+    //if format is 'link' add linkurl key to the selected node
     if (linkUrl) {
         Transforms.setNodes(
             editor,
@@ -256,8 +311,10 @@ const toggleFormat = (editor, format, linkUrl = null) => {
 
 }
 
+//format titles/subtitles within the slate editor
 const formatTitle = (event, editor) => {
     event.preventDefault();
+    //check current selection type to get next title type to toggle to
     const formatSelection = titleFormatSelector(editor);
     // Toggle the block type depending on whether there's already a match.
     Transforms.setNodes(
@@ -267,6 +324,7 @@ const formatTitle = (event, editor) => {
     );
 }
 
+//check current selection type
 const titleFormatSelector = (editor) => {
     let [match] = Editor.nodes(editor, {
         match: n => n.type === 'title',
@@ -286,6 +344,8 @@ const titleFormatSelector = (editor) => {
     return 'title';
 }
 
+//change the type of a block
+//possible types: 'paragraph', 'code', 'title', 'subtitle', 'image'
 const formatBlock = (editor, formatType) => {
     const isActive = isTypeActive(editor, formatType);
     Transforms.setNodes(editor,
@@ -295,6 +355,7 @@ const formatBlock = (editor, formatType) => {
 
 }
 
+//returns if the given type is active or not
 const isTypeActive = (editor, formatType) => {
     const [match] = Editor.nodes(editor, {
         match: n => n.type === formatType
@@ -302,7 +363,7 @@ const isTypeActive = (editor, formatType) => {
 
     return !!match;
 }
-
+//returns if the given format is active or not
 const isFormatActive = (editor, format) => {
     const [match] = Editor.nodes(editor, {
         match: n => n[format] === true,
