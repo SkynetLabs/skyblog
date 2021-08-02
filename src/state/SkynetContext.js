250c6068c6ba5942bd4411736e4283d586b0cae5
Kevin Carlson <kevincarlson@Kevins-MacBook-Pro.local>
2021-08-02 18:28:36 -0500

    update skynet context for profile, feed, and social dac usage

--- src/state/SkynetContext.js
+++ src/state/SkynetContext.js
@@ -1,9 +1,9 @@
 import { createContext, useState, useEffect } from "react";
 import { SkynetClient } from "skynet-js";
 
-// To import DAC, uncomment here, and 2 spots below.
-// import { ContentRecordDAC } from '@skynetlabs/content-record-library';
 import { UserProfileDAC } from "@skynethub/userprofile-library";
+import { FeedDAC } from "feed-dac-library";
+import { SocialDAC } from "social-dac-library";
 
 const SkynetContext = createContext(undefined);
 
@@ -12,6 +12,9 @@ const SkynetContext = createContext(undefined);
 const portal =
   window.location.hostname === "localhost" ? "https://siasky.net" : undefined;
 
+//host skapp
+const hostApp = "skyblog.hns";
+
 // Initiate the SkynetClient
 const client = new SkynetClient(portal);
 
@@ -19,56 +22,132 @@ const client = new SkynetClient(portal);
 // const contentRecord = new ContentRecordDAC();
 const contentRecord = null;
 const userProfile = new UserProfileDAC();
+const feedDAC = new FeedDAC();
+const socialDAC = new SocialDAC();
 
 const dataDomain =
-  window.location.hostname === "localhost" ? "localhost" : "appName.hns";
+  window.location.hostname === "localhost" ? "localhost" : "skyblog.hns";
 
 const SkynetProvider = ({ children }) => {
-  const [skynetState, setSkynetState] = useState({
-    client,
-    mySky: null,
-    contentRecord,
-    userProfile,
-    dataDomain,
-  });
+  const [isMySkyLoading, setMySkyLoading] = useState(true); // state to inidicate whether MySky has loaded
+
+  const [userID, setUserID] = useState(null);
+  const [mySky, setMySky] = useState(null);
+  const [profile, setProfile] = useState(null);
+  const [userPreferences, setUserPreferences] = useState(null);
+  const [userFeed, setUserFeed] = useState(null);
 
   useEffect(() => {
-    // define async setup function
-    async function initMySky() {
-      try {
-        // load invisible iframe and define app's data domain
-        // needed for permissions write
-        const mySky = await client.loadMySky(dataDomain, {
-          debug: true,
-          // dev: true,
-        });
+    initMySky();
+  }, []);
 
-        // load necessary DACs and permissions
-        // Uncomment line below to load DACs
-        // await mySky.loadDacs(contentRecord);
-        await mySky.loadDacs(userProfile);
+  //initialization of mySky and DACs, check log in status
+  const initMySky = async () => {
+    try {
+      const mySky = await client.loadMySky(hostApp);
 
-        // replace mySky in state object
-        setSkynetState({ ...skynetState, mySky });
-      } catch (e) {
-        console.error(e);
+      //load in user profile, feed, and social DACs
+      await mySky.loadDacs([userProfile, feedDAC, socialDAC]);
+
+      const checkLogIn = await mySky.checkLogin();
+      setMySky(mySky);
+
+      if (checkLogIn) {
+        loginActions(mySky);
+      } else {
+        setMySkyLoading(false);
       }
+    } catch (e) {
+      console.log(e);
+      setMySkyLoading(false);
     }
+  };
 
-    // call async setup function
-    if (!skynetState.mySky) {
-      initMySky();
-    }
-
-    return () => {
-      if (skynetState.mySky) {
-        skynetState.mySky.destroy();
+  //function to call on login button press
+  const initiateLogin = async () => {
+    mySky.requestLoginAccess().then((response) => {
+      if (response) {
+        setMySkyLoading(true);
+        loginActions(mySky);
       }
-    };
-  }, [skynetState]);
+    });
+  };
+
+  //actions to perform on successful login check or initial user login
+  const loginActions = async (mySky) => {
+    const userID = await mySky.userID();
+    getUserPreferences(userID);
+    const profile = await getUserProfile(userID);
+    setProfile(profile);
+    setUserID(userID);
+    setMySkyLoading(false); //mySky done loading, change state
+    //fetch feed after profile loaded
+    const feed = await getUserFeed(userID);
+    setUserFeed(feed);
+  };
+
+  //get the current user's global preferences to set DarkMode
+  const getUserPreferences = async (userID) => {
+    const preferences = userProfile.getPreferences(userID);
+    setUserPreferences(preferences);
+  };
+
+  //get a specified user's profile information from the profile DAC
+  const getUserProfile = async (userID) => {
+    const prof = await userProfile.getProfile(userID);
+    return prof;
+  };
+
+  //function to load a specified user's feed of posts
+  const getUserFeed = async (userID) => {
+    const feed = await feedDAC.loadPostsForUser(userID);
+    return feed;
+  };
+
+  //function to upload blog post to the feedDAC
+  //Assumes postJSON is in SkyStandard Post format (for blog post)
+  //Embedded images already uploaded to skynet and links within the markdown
+  const createBlogPost = async (postJSON) => {
+    const res = await feedDAC.createPost(postJSON);
+    return res;
+  };
+
+  //get the following list for a user using the Social DAC
+  const getFollowingList = async (userID) => {
+    const followingList = await socialDAC.getFollowingForUser(userID);
+    return followingList;
+  };
+  //follow a user with id, userID
+  const followUser = async (userID) => {
+    const res = await socialDAC.follow(userID);
+    return res;
+  };
+
+  const mySkyLogout = () => {
+    mySky.logout();
+    setUserID("");
+    setProfile(null);
+    setUserFeed(null);
+  };
 
   return (
-    <SkynetContext.Provider value={skynetState}>
+    <SkynetContext.Provider
+      value={{
+        isMySkyLoading,
+        userID,
+        mySky,
+        profile,
+        userPreferences,
+        userFeed,
+        getUserProfile,
+        getUserFeed,
+        createBlogPost,
+        getFollowingList,
+        followUser,
+        initiateLogin,
+        mySkyLogout,
+      }}
+    >
       {children}
     </SkynetContext.Provider>
   );
