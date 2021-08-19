e552c76039b3a76d48d8c20340068bcb7bbf7c86
Kevin Carlson <kevin.c.carlson1@gmail.com>
2021-08-18 21:41:37 -0500

    basic info on homepage

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,30 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Typography from "@material-ui/core/Typography";
 import AppBar from "@material-ui/core/AppBar";
 import IconButton from "@material-ui/core/IconButton";
 import Toolbar from "@material-ui/core/Toolbar";
 import { makeStyles } from "@material-ui/core/styles";
-import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import AccountCircle from "@material-ui/icons/AccountCircle";
-import Search from "@material-ui/icons/Search";
 import Add from "@material-ui/icons/Add";
 import { Link } from "react-router-dom";
-
-function ElevationScroll(props) {
-  const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 0,
-    target: window ? window() : undefined,
-  });
-
-  return React.cloneElement(children, {
-    elevation: trigger ? 4 : 0,
-  });
-}
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -37,42 +19,32 @@ export default function NavigationBar(props) {
 
   return (
     <div className={classes.root}>
-      <ElevationScroll {...props}>
-        <AppBar position={"static"}>
-          <Toolbar>
-            <Typography variant="h6" className={classes.root}>
-              SkyBlog
-            </Typography>
-            <div>
-              <IconButton
-                edge="end"
-                aria-label="search"
-                aria-haspopup="true"
-                color="inherit"
-              >
-                <Search />
-              </IconButton>
-              <IconButton
-                component={Link}
-                to={"/create"}
-                edge="end"
-                aria-label="create"
-                color="inherit"
-              >
-                <Add />
-              </IconButton>
-              <IconButton
-                edge="end"
-                aria-label="account"
-                aria-haspopup="true"
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-            </div>
-          </Toolbar>
-        </AppBar>
-      </ElevationScroll>
+      <AppBar position={"static"}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.root}>
+            SkyBlog
+          </Typography>
+          <div>
+            <IconButton
+              component={Link}
+              to={"/create"}
+              edge="end"
+              aria-label="create"
+              color="inherit"
+            >
+              <Add />
+            </IconButton>
+            <IconButton
+              edge="end"
+              aria-label="account"
+              aria-haspopup="true"
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,85 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext } from "react";
 import Container from "@material-ui/core/Container";
-import { AccountCircle } from "@material-ui/icons";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
+import Box from "@material-ui/core/Box";
+import { SkynetContext } from "../state/SkynetContext";
+import { Link } from "react-router-dom";
 
 export default function Home(props) {
+  const { userID, initiateLogin } = useContext(SkynetContext); //use SkynetContext to determine Login status
+
+  //Function to open Skynet homepage in new tab
+  const openSkynetHomePage = () => {
+    window.open("https://siasky.net", "_blank");
+  };
+
+  //Render basic information for user on homepage
   return (
-    <Container maxWidth={"md"}>
-      <p>Hello home</p>
+    <Container maxWidth={false} style={{ padding: 20 }}>
+      <Box display={"flex"} justifyContent={"center"}>
+        <Typography variant={"h3"} gutterBottom={true}>
+          Share your story using the new decentralized internet.
+        </Typography>
+      </Box>
+      <Box display={"flex"} justifyContent={"center"}>
+        {userID != null && userID != "" ? (
+          <Button
+            size={"large"}
+            color={"primary"}
+            variant={"contained"}
+            style={{ width: "30%" }}
+            component={Link}
+            to={"/create"}
+          >
+            Get started
+          </Button>
+        ) : (
+          <Button
+            size={"large"}
+            color={"primary"}
+            variant={"contained"}
+            style={{ width: "30%" }}
+            onClick={initiateLogin}
+          >
+            Login with MySky
+          </Button>
+        )}
+      </Box>
+
+      <Divider style={{ margin: 28 }} />
+
+      <Box display={"flex"} justifyContent={"center"} style={{ margin: 20 }}>
+        <img
+          src={
+            "https://siasky.net/NABosXa1JcBQngZeA4e43JgSfQti1nadqjMKPibH6cgcNg"
+          }
+          style={{ maxHeight: window.innerHeight * 0.15 }}
+        />
+      </Box>
+
+      <Box display={"flex"} justifyContent={"center"}>
+        <Typography variant={"h3"}>
+          Learn more about how your blog is hosted on Skynet.
+        </Typography>
+      </Box>
+      <Box display={"flex"} justifyContent={"center"}>
+        <Typography variant={"h6"} style={{ color: "gray" }}>
+          Skynet prioritizes privacy and security to empower users.
+        </Typography>
+      </Box>
+      <Box display={"flex"} justifyContent={"center"} style={{ margin: 20 }}>
+        <Button
+          size={"large"}
+          color={"primary"}
+          variant={"contained"}
+          style={{ width: "30%" }}
+          onClick={openSkynetHomePage}
+        >
+          Learn More
+        </Button>
+      </Box>
     </Container>
   );
 }
